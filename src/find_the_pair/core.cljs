4aca05f3f9d9cabb21f193cd166334944e7a3517
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-04-07 21:35:22 +0300

    Refactored rows and columns to coordinates x and y. Removed unnecessary function random-card.

--- src/find_the_pair/core.cljs
+++ src/find_the_pair/core.cljs
@@ -3,18 +3,11 @@
 
 (enable-console-print!)
 
+(defn fill-board-with-cards [board cards]
+  (map (fn [[x1 x2] [y1 y2]] [y1 y2]) board cards))
 
-(def card-size 0.95)
-(def grid-width 2)
-(def grid-height 3)
-
-(defn random-card [card-numbers]
-  (let [random-number (rand-int (- (count card-numbers) 1))
-        chosen-number (nth card-numbers random-number)]
-    chosen-number))
-
-(defn init-cards []
-  (let [pair-amount (range 1 (+ (/ (* grid-width grid-height) 2) 1))
+(defn generate-cards [width height]
+  (let [pair-amount (range 1 (+ (/ (* width height) 2) 1))
         card-numbers (apply concat (map (fn [x] [x x]) pair-amount))
         card-numbers-partitioned (partition 2 (shuffle card-numbers))]
     card-numbers-partitioned))
@@ -22,13 +15,12 @@
 (defn generate-board [width height]
   (vec (repeat height (vec (repeat width 0)))))
 
-(defn fill-board-with-cards [board cards]
-  (map (fn [[x1 x2] [y1 y2]] [y1 y2]) board cards))
-
 (defn init-board [width height]
-  (fill-board-with-cards (generate-board width height) (init-cards)))
+  (fill-board-with-cards (generate-board width height) (generate-cards width height)))
 
-(prn (init-board grid-width grid-height))
+(def grid-width 2)
+(def grid-height 3)
+(def card-size 0.95)
 
 (def initial-app-state
   {:text "Find the pair!"
@@ -39,15 +31,18 @@
 (defonce app-state
   (atom initial-app-state))
 
-(defn card-value [row column]
-  (get-in @app-state [:board row column]))
+
+;; ===== UI =====
+
+(defn card-value [x y]
+  (get-in @app-state [:board x y]))
 
 (defn turn-value []
   (get-in @app-state [:turn]))
 
-(defn set-selected-cards! [row column]
+(defn set-selected-cards! [x y]
   (if (< (turn-value) 2)
-    (swap! app-state assoc-in [:selected-cards (turn-value)] [row column])
+    (swap! app-state assoc-in [:selected-cards (turn-value)] [x y])
     (swap! app-state assoc-in [:selected-cards] [[nil nil] [nil nil]])))
 
 (defn set-turn! []
@@ -60,9 +55,9 @@
   (prn (:selected-cards @app-state))
   #_(prn (:turn @app-state)))
 
-(defn selected-card? [row column]
-  (or (= [row column] (get-in @app-state [:selected-cards 0]))
-      (= [row column] (get-in @app-state [:selected-cards 1]))))
+(defn selected-card? [x y]
+  (or (= [x y] (get-in @app-state [:selected-cards 0]))
+      (= [x y] (get-in @app-state [:selected-cards 1]))))
 
 (defn find-the-pair []
   [:div
@@ -72,19 +67,19 @@
       {:view-box (str "0 0 " grid-width " " grid-height)
        :width 500
        :height 500}]
-     (for [column (range grid-width)
-           row (range grid-height)]
-       [:rect {:x column
-               :y row
+     (for [x (range grid-width)
+           y (range grid-height)]
+       [:rect {:x x
+               :y y
                :width card-size
                :height card-size
-               :fill (if (selected-card? row column)
+               :fill (if (selected-card? x y)
                        "#00ABE1"
                        "#ccc")
                :on-click
                (fn card-click [e]
                  ()
-                 (set-selected-cards! row column)
+                 (set-selected-cards! x y)
                  (set-turn!)
                  (debug-state))}]))])
 
