e99784b2f194c5eb8bbb3a1777bf405c68f43d7d
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-04-07 21:12:15 +0300

    Cards have now value and they have also pair with same value.

--- src/find_the_pair/core.cljs
+++ src/find_the_pair/core.cljs
@@ -3,24 +3,41 @@
 
 (enable-console-print!)
 
+
 (def card-size 0.95)
-(def grid-width 6)
-(def grid-height 6)
+(def grid-width 2)
+(def grid-height 3)
 
-(defn init-cards [board]
-  board)
+(defn random-card [card-numbers]
+  (let [random-number (rand-int (- (count card-numbers) 1))
+        chosen-number (nth card-numbers random-number)]
+    chosen-number))
 
-(defn new-board [width height]
-  (let [board (vec (repeat height (vec (repeat width 0))))]
-    (init-cards board)))
+(defn init-cards []
+  (let [pair-amount (range 1 (+ (/ (* grid-width grid-height) 2) 1))
+        card-numbers (apply concat (map (fn [x] [x x]) pair-amount))
+        card-numbers-partitioned (partition 2 (shuffle card-numbers))]
+    card-numbers-partitioned))
 
-(prn (new-board grid-width grid-height))
+(defn generate-board [width height]
+  (vec (repeat height (vec (repeat width 0)))))
+
+(defn fill-board-with-cards [board cards]
+  (map (fn [[x1 x2] [y1 y2]] [y1 y2]) board cards))
+
+(defn init-board [width height]
+  (fill-board-with-cards (generate-board width height) (init-cards)))
+
+(prn (init-board grid-width grid-height))
+
+(def initial-app-state
+  {:text "Find the pair!"
+   :board (init-board grid-width grid-height)
+   :selected-cards [[nil nil] [nil nil]]
+   :turn 0})
 
 (defonce app-state
-  (atom {:text "Find the pair!"
-         :board (new-board grid-width grid-height)
-         :selected-cards [[nil nil] [nil nil]]
-         :turn 0}))
+  (atom initial-app-state))
 
 (defn card-value [row column]
   (get-in @app-state [:board row column]))
@@ -42,10 +59,6 @@
   (prn (:board @app-state))
   (prn (:selected-cards @app-state))
   #_(prn (:turn @app-state)))
-
-(defn change-board-state [row column]
-  (swap! app-state assoc-in [:board row column]
-         (inc (card-value row column))))
 
 (defn selected-card? [row column]
   (or (= [row column] (get-in @app-state [:selected-cards 0]))
@@ -70,11 +83,9 @@
                        "#ccc")
                :on-click
                (fn card-click [e]
-                 ;;(prn "You clicked card" column row)
                  ()
                  (set-selected-cards! row column)
                  (set-turn!)
-                 (change-board-state row column)
                  (debug-state))}]))])
 
 (reagent/render-component [find-the-pair]
