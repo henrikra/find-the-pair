72f17eeef43368026633ca67b43bbaec38f956cd
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-13 02:32:16 +0300

    Move card-exists? function higher so more if statements can be removed

--- src/find_the_pair/card.cljs
+++ src/find_the_pair/card.cljs
@@ -11,19 +11,17 @@
   (let [card-side (/ init/container-width (cards-per-row))]
     [:div.card {:style {:width (str card-side "px")
                         :height (str card-side "px")}}
-     [:div {:class (if (flipped-card? x y)
+     (if (card-exists? x y)
+       [:div {:class (if (flipped-card? x y)
                      "card__sides card__sides--flipped"
                      "card__sides")
             :on-click (fn []
                         (if (and (card-exists? x y)
                                  (not (flipped-card? x y))
                                  (not (both-cards-flipped?)))
-                          (set-flipped-card x y)))
-            :style (if (card-exists? x y)
-                     {:cursor "pointer"})}
-      [:div.card__side.card__back {:style (if (not (card-exists? x y))
-                                            {:background "#ecf0f1"})}]
-      [:div.card__side.card__front
-       (if (flipped-card? x y)
-         [:i {:class (card-icon x y)
-              :style {:font-size (str (* card-side 0.4) "px")}}])]]]))
+                          (set-flipped-card x y)))}
+        [:div.card__side.card__back]
+        [:div.card__side.card__front
+         (if (flipped-card? x y)
+           [:i {:class (card-icon x y)
+                :style {:font-size (str (* card-side 0.4) "px")}}])]])]))
