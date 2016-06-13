2f71c4e0b45972b54c7224e702aa6794acb21d05
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-06-13 03:44:18 +0300

    Move container styles to clojurescript file

--- src/find_the_pair/app.cljs
+++ src/find_the_pair/app.cljs
@@ -1,12 +1,12 @@
 (ns find-the-pair.app
-  (:require [find-the-pair.init :as init]
-            [find-the-pair.difficulty-dropdown :refer [difficulty-dropdown]]
+  (:require [find-the-pair.difficulty-dropdown :refer [difficulty-dropdown]]
             [find-the-pair.victory-view :refer [victory-view]]
             [find-the-pair.state :refer [game-won?]]
-            [find-the-pair.board-view :refer [board-view]]))
+            [find-the-pair.board-view :refer [board-view]]
+            [find-the-pair.style :as style]))
 
 (defn app []
-  [:div.container {:style {:max-width init/container-width}}
+  [:div.container {:style style/container}
    [:h1 "Find the pair!"]
    (difficulty-dropdown)
    (if (game-won?)
---
+++ src/find_the_pair/style.cljs
@@ -0,0 +1,6 @@
+(ns find-the-pair.style
+  (:require [find-the-pair.init :as init]))
+
+(def container
+  {:max-width init/container-width
+   :margin "0 auto"})
